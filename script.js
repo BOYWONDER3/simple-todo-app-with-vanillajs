0eea401adb560ed0e9731c2b9b8cb2156a59e9e4
boywonder3 <opeyemiisaacboywonder@gmail.com>
2022-12-04 09:16:39 +0100

    second commit

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
-// select all elments 
+// 1. Select all elements
 const form = document.querySelector("#new-item-form")
-const list = document.querySelector('#list')
-const input = document.querySelector('#item-input')
+const list = document.querySelector("#list")
+const input = document.querySelector("#item-input")
 
-// when i submit the form, add a new element
-
+// 2. When I submit the form add a new element
 form.addEventListener("submit", e => {
-    e.preventDefault()
+  e.preventDefault()
 
-    // create a new item 
-    const item = document.createElement('div')
-    item.innerText = input.value
-    item.classList.add("list-item")
+  // 1. Create a new item
+  const item = document.createElement("div")
+  item.innerText = input.value
+  item.classList.add("list-item")
 
-    // add that item to the list
-    list.appendChild(item)
+  // 2. Add that item to the list
+  list.appendChild(item)
 
-    // clear input
-    input.value = ""
+  // 3. Clear input
+  input.value = ""
 
-    // setup event listner to delete item when clicked
-    item.addEventListener('click', () => {
-        item.remove()
-    })
-
+  // 4. Setup event listener to delete item when clicked
+  item.addEventListener("click", () => {
+    item.remove()
+  })
 })
